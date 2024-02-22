815505d26728eb442036647dd668297a0f807eba
Leow Kean Tat <leowkeantat@gmail.com>
2024-02-22 13:14:58 +0800

    Refactored the timer implementation to resume from whence it was paused (down to the millisecond).

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -15,24 +15,64 @@ export default function TodoCard({ todo }) {
     const setShowDeleteTodoModal = GetTodoContext().setShowDeleteTodoModal;
     const handleShow = () => setShowDeleteTodoModal({ visible: true, todoID: todo.id });
 
+    let isPausedRef = useRef(false);
+
+    let timeStartedRef = useRef(Date.now());
+    let timeOffsetRef = useRef(0);
+
     const startTimer = () => {
         if (!timerInterval) {
+            timeStartedRef.current = Date.now();
+
+            // Debug
+            //console.log("[Init - Started At] Time (Since Unix): " + timeStartedRef.current);
+
             const intervalID = setInterval(() => {
-                setTimer((prevTimer) => prevTimer + 1);
-            }, 1000)
+                if (isPausedRef.current)
+                    return;
+
+                // Current Time - Time Started = Total Duration.
+                // Total Duration - (Resumed Time  - Paused Time) = Remaining Duration when it was in "played" state.
+                timeOffsetRef.current += Date.now() - timeStartedRef.current;
+                setTimer(Math.round(timeOffsetRef.current / 1000));
+
+                // Debug
+                //console.log("[Timer Update] Time Offset, Now - Start (Since Unix): " + timeOffsetRef.current);
+                //console.log("[Timer Update] Time Started: " + timeStartedRef.current);
+                //console.log("[Timer Update] Time Now: " + Date.now());
+
+                timeStartedRef.current = Date.now();
+            }, 100)
 
             setTimerInterval(intervalID);
+        }
+        else {
+            timeStartedRef.current = Date.now();
+            isPausedRef.current = false;
         }
     };
 
     const pauseTimer = () => {
-        clearInterval(timerInterval);
-        setTimerInterval(null);
+        if (!isPausedRef.current) {
+            isPausedRef.current = true;
+
+            timeOffsetRef.current += Date.now() - timeStartedRef.current;
+
+            // Debug
+            //console.log("[Timer Paused] Time Offset, Now - Start (Since Unix): " + timeOffsetRef.current);
+        }
+
+        // No longer remove timer on delete.
+        //clearInterval(timerInterval);
+        //setTimerInterval(null);
     };
 
     const resetTimer = () => {
         clearInterval(timerInterval);
         setTimerInterval(null);
+
+        isPausedRef.current = false;
+        timeOffsetRef.current = 0;
         setTimer(0);
     };
 
